934485981cb60296a6a84f2c4d2bcec1f4588eb8
Samuel Cornet <samuel@noukod.com>
2019-02-04 18:00:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 // Code your solution in this file
 function findMatching(collection, name) {
   for (const drivers of collection) {
-    if (drivers.name == name)
-    console.log(drivers.name);
+    if (drivers.name ===name){ 
+      console.log(drivers.name);
+    else if (drivers !==name) {
+      console.log(" ");
+    }
+    
   }
 }
 
+else if (drivers) !==name);
 // function blueFilter (collection) {
 //   for (const user of collection) {
 //     if (user.favoriteColor === 'Blue') {

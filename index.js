cbc203cb1b675905ed73f584e380e6f98dac04d6
Samuel Cornet <samuel@noukod.com>
2019-02-04 18:50:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ function findMatching(drivers, fullname){
 }
 
 function fuzzyMatch(drivers, chars) {
- return drivers.filter(function (driver){return driver.slice(0, chars.length) == partialWrold});
+ return drivers.filter(function (driver){return driver.slice(0, chars.length) == chars});
 }

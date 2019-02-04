593b4ea7025428a4a16af163c51288dcc9cd82db
Samuel Cornet <samuel@noukod.com>
2019-02-04 17:53:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 // Code your solution in this file
-function findMatching() {
+function findMatching(collection) {
+  for (const drivers of collection) {
+    console.log(drivers.name);
+  }
+}
 
+function firstNamePrinter (collection) {
+  for (const user of collection) {
+    console.log(user.firstName);
+  }
 }

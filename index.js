dea12d44f7a368c87733a25e9a50d22c4e62a244
Samuel Cornet <samuel@noukod.com>
2019-02-04 18:14:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // Code your solution in this file
-function filter (collection) {
-  for (const drivers of collection) {
-    if (findMatching(drivers, attribute, value)) {
-      console.log(drivers.attribute);
-    }
-  }
-}
-
-function findMatching (drivers) {
-  return user.attribute === value;
-}
-
-
-
-
-// function findMatching(drivers, word){
-//     return drivers.filter(driver => driver.toLowerCase() == word.toLowerCase());
+// function filter (collection) {
+//   for (const drivers of collection) {
+//     if (findMatching(drivers, attribute, value)) {
+//       console.log(drivers.attribute);
+//     }
+//   }
 // }
+// 
+// function findMatching (drivers) {
+//   return user.attribute === value;
+// }
+
+
+
+
+function findMatching(drivers, word){
+    return drivers.filter(driver => driver.toLowerCase() == word.toLowerCase());
+}

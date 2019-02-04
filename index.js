a1656ec508470d21cbf193e24f654af676777809
Samuel Cornet <samuel@noukod.com>
2019-02-04 18:43:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function findMatching(drivers, fullname){
     return drivers.filter(driver => driver.toLowerCase() === fullname.toLowerCase());
 }
 
-function fuzzyMatch(drivers, char) {
+function fuzzyMatch(drivers, chars) {
   // const filtreTexte = (requete) => {
   return drivers.filter(char =>  char.toLowerCase().indexOf(filterText.toLowerCase()) == char> 0);
-
+ return drivers.filter(function (driver){return driver.slice(0, partialWrold.length) == partialWrold});
 }
